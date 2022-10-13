adf8858d6234942ab989f70b1962fa6599827d79
qiuwuxue <112790260+qiuwuxue@users.noreply.github.com>
2022-10-13 17:59:08 -0500

    S39: basic CRUD

---
+++ seeds/index.js
@@ -0,0 +1,31 @@
+
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers')
+const Campground = require('../models/campground');
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/yelpcamp')
+const db = mongoose.connection
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", () => {
+    console.log("Database connected")
+})
+
+// a function return a random element of an array
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({})
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
